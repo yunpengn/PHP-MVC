7dd425e64cd94bd96fdb4420c2e970cfd16c471f
Yunpeng Niu <neilniuyunpeng@gmail.com>
2018-05-10 23:29:19 +0800

    Include index.php

---
+++ index.php
@@ -0,0 +1,28 @@
+<?php session_start(); ?>
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <?php include "public/head.php"; ?>
+</head>
+<body>
+<?php
+/**
+ * Acts as a router with a facade for the whole application.
+ */
+require_once 'core/App.class.php';
+require_once 'config/config.php';
+require_once 'vendor/autoload.php';
+
+// Utilizes auto-loading here.
+spl_autoload_register(array("App", "myAutoLoader"));
+
+// Runs an instance of the main app delegate.
+try {
+    App::run($_GET['url']);
+} catch(NotFoundException $nfe) {
+    $nfe->showError();
+}
+?>
+</body>
+</html>
+
